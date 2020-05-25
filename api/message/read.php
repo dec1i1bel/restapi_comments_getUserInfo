95f1ab45e00a557add7ad6d1c8c00dea614ac611
vaba2660 <v.balabanov85@gmail.com>
2020-05-25 19:59:58 +0300

    .

--- api/message/read.php
+++ api/message/read.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json; charset=UTF-8');
 
 include '../api/config/connectDb.php';
@@ -15,10 +15,20 @@ $mess_num = 1;
 
 if($mess_num>0) {
   $arrMessages = array();
-  $arrMessages['records'] = array();
+  // $arrMessages['records'] = array();
   while($row = $messages->fetch()) {
-    echo '<p>'.print_r($row).'</p>';
+    // echo '<p>'.print_r($row).'</p>';
+    extract($row);
+    $message_item = array(
+      'c_message' => $c_message,
+      'c_publicationDate' => $c_publicationDate,
+      'c_status' => $c_status,
+      'c_userId' => $c_userId
+    );
+    array_push($arrMessages, $message_item);
   }
+  http_response_code(200);
+  echo json_encode($arrMessages);
 } else {
   http_response_code(404);
   echo json_encode(array('message' => 'У пользователя нет сообщений'));
--- session.sql
+++ session.sql
@@ -22,7 +22,7 @@
 -- describe comments
 -- insert into comments (status, publicationDate, message, userId)
 -- values ('опубликовано', '2020-05-20', 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 1)
-select * from users
+-- select * from users
 -- describe users
 -- insert into users (name,birthdate,city,phone,photo)
 -- values ('иванов иван иванович','1990-07-29','Владивосток','+7(123) 456-78-90', 'img/user.jpg');
